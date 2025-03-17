0468d11bb6b249265453c47801de68e5b817fe2d
Limebro123 <limebro@hotmail.com>
2025-03-17 08:31:24 +0100

    Uploaded poster screne med script

---
+++ Programmerings Kinesisk Propaganda Spil/Assets/Emils folder/poster.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class poster : MonoBehaviour
+{
+    bool isActive = false;
+    public GameObject[] t;
+    // Start is called before the first frame update
+    void Start()
+    {
+        t[0].SetActive(false);
+        t[1].SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+  
+
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+       
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+            if (other.gameObject.CompareTag("pop"))
+            {
+                int temp = int.Parse(other.gameObject.name);
+                isActive = !isActive;
+                t[temp].SetActive(isActive);
+
+            }
+
+        }
+    }
+}
