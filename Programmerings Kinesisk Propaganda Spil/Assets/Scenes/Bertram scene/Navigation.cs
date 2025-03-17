a786bd1676c025995cdefba8d462e4ab3cb767f9
Flamer8010 <129384566+Flamer8010@users.noreply.github.com>
2025-03-17 14:57:56 +0100

    Random npc movement

--- Programmerings Kinesisk Propaganda Spil/Assets/Scenes/Bertram scene/Navigation.cs
+++ Programmerings Kinesisk Propaganda Spil/Assets/Scenes/Bertram scene/Navigation.cs
@@ -1,22 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 using UnityEngine.AI;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    public Transform player;
+    public Transform[] player;
     private NavMeshAgent agent;
+    int targetPoint = -1;
 
     // Start is called before the first frame update
     void Start()
     {
-        agent = GetComponent<NavMeshAgent>();  
+        agent = GetComponent<NavMeshAgent>();
+        targetPoint = (int)Random.Range(0, player.Length);
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.destination = player.position; 
+        agent.destination = player[targetPoint].position; 
     }
 }
